b4c9bcee9ef6898e38746f455344ace53c58be23
Iago da Conceição Barbosa <iago-cb@hotmail.com>
2022-10-25 22:04:13 -0300

    Adiciona crud de vendas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PacienteController;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\VendasController;
 
 Route::get('/imc/{nome}/{peso}/{altura}/{genero}', [PacienteController::class, 'calcularIMC']);
-Route::get('/{tipo?}', [ProdutoController::class, 'listarProdutos']);
+/*Route::get('/{tipo?}', [ProdutoController::class, 'listarProdutos']);*/
+
+
+Route::prefix('vendas')->group(function () {
+    Route::get('/', [VendasController::class,'listarVendas']);
+    Route::get('/ver/{id}', [VendasController::class,'verVenda']);
+    Route::get('/nova/{produto}/{preco}/{quantidade}', [VendasController::class, 'cadastrarVenda']);
+    Route::get('/atualizar/{id}/{produto}/{preco}/{quantidade}', [VendasController::class,'atualizarVenda']);
+    Route::get('/excluir/{id}', [VendasController::class,'excluirVenda']);
+});
